321e416ccb6b4b3ba63070274fbc057b17bfc458
Nikolic-Marko <nikolicmarko653@gmail.com>
2021-04-05 15:18:31 +0200

    Module 2 update

--- src/components/BookItem.vue
+++ src/components/BookItem.vue
@@ -1,13 +1,20 @@
 <template>
   <li>
     {{book.title}} : {{book.author}}
+    <span v-if="book.finishReading == true">Read</span>
+    <span v-else>Not Read</span>
   </li>
 </template>
 
 <script>
+import BookList from './BookList'
 export default {
+  
   name: 'BookItem',
   props: ['book'],
+  components:{
+    BookList,
+  }
 };
 </script>
 
